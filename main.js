167bcd83fb0dfa39eb60033d36d48fd102e0b038
unknown <marius.proton@gmail.com>
2023-05-02 18:51:14 +0200

    wip firebase working

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, remove, onValue, push, set } from "firebase/database";
+import { getDatabase, ref, remove, onValue, push, update, child } from "firebase/database";
+
+import QRCode from 'qrcode';
 
 import * as desktop from './desktop'
 import * as mobile from './mobile'
-
-mobile.init(window.location.search)
 
 const firebaseConfig = {
   databaseURL: "https://webgl-ed2ec-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
+let currentSession = null
+
+//firebase config
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
 const sessions = ref(database, 'sessions/');
+
+//envent on sessions modified
 onValue(sessions, (snapshot) => {
   const data = snapshot.val();
   
   //delete old sessions
   Object.keys(data).map((key) => {
     let item = data[key]
-    if (item.creationDate < Date.now() - 30000) {
+    if (item.creationDate < Date.now() - 30000){
       remove(ref(database, 'sessions/' + key))
     }
   })
@@ -29,20 +33,60 @@ onValue(sessions, (snapshot) => {
   mobile.displayList(data)
 });
 
-function createSession(messages, creationDate) {
-  push(ref(database, 'sessions/'), {
-    messages,
-    creationDate
-  });
+function createSession(){
+  currentSession = push(ref(database, 'sessions/'), {
+    creationDate: Date.now()
+  }).key;
+  push(ref(database, `sessions/${currentSession}/messages/`), {
+    msg : "bonjour"
+  })
+
+  //DEBUG display session
+  let p = document.createElement('p')
+  p.textContent = currentSession.slice(17)
+  document.getElementById('container').appendChild(p)
+  displayQrCode(currentSession)
 }
 
-createSession(["coucou", "je"], Date.now())
+function displayQrCode(key){
+  var canvas = document.getElementById('qrcode')
+  QRCode.toCanvas(canvas, `http://192.168.130.19:5173/?${key}`, function (error){
+    if (error) console.error(error)
+  })
+}
 
+//callback on desktop to transmit events to backend
+function handleDesktopEvent(event){
+  console.log('event', event)
+  if (event === "green zone"){
+    push(ref(database, `sessions/${currentSession}/messages/`), {
+      msg : "green zone message"
+    })
+  }
+}
 
-if (/Android|iPhone/i.test(navigator.userAgent)) {
+// function getData(id){
+//   return new Promise(resolve => {
+//     get(ref(database, `sessions/${id}/messages/`)).then((snapshot) => {
+//       if (snapshot.exists()){
+//         setTimeout(() => {
+          
+//           resolve(snapshot.val());
+//         }, 1000);
+//       }
+//     }).catch((error) => {
+//       resolve(error)
+//     });
+//   })
+// }
+
+if (/Android|iPhone/i.test(navigator.userAgent)){  //client is mobile
   document.getElementById('container').remove()
-}else{
+  mobile.createMobileInterface(window.location.search)
+}else{                                              //client is desktop
   document.getElementById('mobile').remove()
+  desktop.createWebGl(handleDesktopEvent)
+  createSession()
 }
 
 
--- mobile.js
+++ mobile.js
@@ -1,26 +1,30 @@
 let container = document.getElementById('mobile')
 let ul = document.createElement('ul')
 container.appendChild(ul)
-
 let sessionId = null
 
-export function init(id) {
+export async function createMobileInterface(id){
     if (id.charAt(0) !== "?"){
         console.log("erreur 404");
     }else {
         console.log(id);
         sessionId = id.slice(1)
+
+        //DEBUG display session
+        let h2 = document.createElement('h2')
+        h2.textContent = sessionId.slice(17)
+        container.appendChild(h2)
     }
 }
 
 export function displayList(list){
-    while (ul.firstChild) {
+    while (ul.firstChild){
         ul.removeChild(ul.firstChild);
     }
-    list[sessionId].messages.map((item) => {
+    Object.values(list[sessionId]?.messages).map((item) => {
         let msg = document.createElement('li')
         msg.classList.add("msg")
-        msg.textContent = item
+        msg.textContent = item.msg
         ul.appendChild(msg)
     })
 }

6e4b6ef360bc6e8f9f516c7318c6bcc5f151b596
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-05-10 13:12:06 +0200

    removed unused imports in floor.js

--- src/Experience/World/GameElements/Floor.js
+++ src/Experience/World/GameElements/Floor.js
@@ -1,8 +1,3 @@
-import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { Octree } from 'three/addons/math/Octree.js';
-import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
-
 export default class Floor {
     constructor(_options) {
         this.scene = _options.scene;

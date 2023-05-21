01be66ee347a2e8a4f8f309f9776b3e84b136012
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-05-21 12:41:43 +0200

    reposition de l'ashgroup

--- src/Experience/World/Rooms/Room2.js
+++ src/Experience/World/Rooms/Room2.js
@@ -33,6 +33,7 @@ export default class Room2 extends Room {
     setAshes() {
         // Créer un groupe pour contenir les particules de cendres
         this.ashGroup = new THREE.Group();
+        this.ashGroup.position.set(2, 2, 6);
         this.props.push(this.ashGroup)
 
         // Créer le matériau des particules de cendres
@@ -50,9 +51,9 @@ export default class Room2 extends Room {
         const positions = [];
         for (let i = 0; i < 1000; i++) {
             positions.push(
-                Math.random() * 100 - 50, // Position X aléatoire
-                Math.random() * 100 - 50, // Position Y aléatoire
-                Math.random() * 100 - 50  // Position Z aléatoire
+                (Math.random() * 2 - 1) * 10, // Position X aléatoire
+                (Math.random() * 2 - 1) * 10, // Position Y aléatoire
+                (Math.random() * 2 - 1) * 10  // Position Z aléatoire
             );
         }
         ashGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
@@ -65,7 +66,7 @@ export default class Room2 extends Room {
     animateAsh() {
         // Mettre à jour la position des particules
         this.ashes.rotation.y += 0.01; // Rotation des particules
-        this.ashGroup.position.y -= 0.1; // Vitesse de descente des particules
+        this.ashGroup.position.y -= 0.05; // Vitesse de descente des particules
 
         // Réinitialiser la position des particules qui sont sorties de la vue
         if (this.ashGroup.position.y < -10) {

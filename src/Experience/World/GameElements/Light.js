3b2c1973a37bacec69bcfdca8b14429463f28ae3
Luca Ciampi <luca.ciampi@edu.gobelins.fr>
2023-05-24 19:55:19 +0200

    :lipstick: Upgraded debug default params

--- src/Experience/World/GameElements/Light.js
+++ src/Experience/World/GameElements/Light.js
@@ -34,23 +34,24 @@ export default class Light {
     setLight() {
         RectAreaLightUniformsLib.init();
 
-        this.spotLight = new THREE.SpotLight(0xffffff, 2)
         this.ambientLight = new THREE.AmbientLight(0xffffff, 0.07)
+        
         this.rectLight = new THREE.RectAreaLight(0xffffff, 25, 0.09, 0.16);
         this.rectLight.position.set(1, 1, -0.7);
         this.rectLight.rotateY(-Math.PI / 2)
-
+        
+        this.spotLight = new THREE.SpotLight(0xffffff, 2)
         this.spotLight.angle = 0.3;
         this.spotLight.penumbra = 0.1;
         this.spotLight.decay = 2;
         this.spotLight.distance = 50;
         this.spotLight.position.y += 1
+        this.spotLight.position.set(0, 0, 1);
+        this.spotLight.target = this.camera.instance;
 
         this.scene.add(this.rectLight);
         this.scene.add(this.ambientLight)
         this.camera.instance.add(this.spotLight)
-        this.spotLight.position.set(0, 0, 1);
-        this.spotLight.target = this.camera.instance;
     }
 
     setFog() {
@@ -61,11 +62,18 @@ export default class Light {
    * Adds debug options on GUI
    */
     addDebugOptions() {
+        const ambientFolder = this.debug.gui.addFolder('Ambient Light');
+        ambientFolder.add(this.ambientLight, 'intensity');
+        
+        const spotLightFolder = this.debug.gui.addFolder('Spotlight');
+        spotLightFolder.add(this.spotLight, 'intensity');
+        
+        const fogFolder = this.debug.gui.addFolder('Fog');
+        fogFolder.add(this.scene.fog, 'far');
+        
         this.scene.fog.far = 100;
-
-        const folder = this.debug.gui.addFolder('Light');
-        folder.add(this.ambientLight, 'intensity');
-        folder.add(this.scene.fog, 'far');
+        this.ambientLight.intensity = 1;
+        this.spotLight.intensity = 0;
     }
 
 }
